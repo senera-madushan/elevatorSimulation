72f099ff04fb834ba4dce7c9310d52f056fa7986
senera-madushan <it16235298@my.sliit.lk>
2020-05-11 16:32:55 +0530

    Add some final variables..

---
+++ src/main/java/com/sliit/elevator/constraints/Utils.java
@@ -0,0 +1,11 @@
+package com.sliit.elevator.constraints;
+
+public class Utils {
+
+    public static final int  noOfFloors =10;
+    public static final int travelTime = 3;
+    public static final int waitTime =4;
+
+
+
+}
